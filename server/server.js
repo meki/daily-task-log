28616b9de18396ff24abce77de9601b3e3f4bfb1
ssashida <sashidapc373443@gmail.com>
2018-04-28 23:09:38 +0900

    first commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+"use strict";
+// Express framework
+const express = require('express');
+const app = express();
+app.set('port', process.env.PORT || 80);
+
+const rootDir = __dirname + "/..";
+
+const server = app.listen(app.get('port'));
+console.log("server listening on port " + app.get('port'));
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
